420d2108efa9dfa71a032a24c563a6a1be417c12
Devonltaylor <devonltaylor22@gmail.com>
2018-02-22 03:26:52 -0500

    Second Commit: Screen split for two teams, currently A and B. Java code is functional for team with buttons working and updates to score. Next commit will be for Team B.

--- MainActivity.java
+++ MainActivity.java
@@ -2,15 +2,46 @@ package com.vonlor.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(10);
+    }
+
+    /**
+     * This Will add three points to Team (display only current)
+     */
+
+    public void addThreeToA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * This Will add two points to Team (display only current)
+     */
+
+    public void addTwoToA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * This Will add one point to Team (display only current)
+     */
+
+    public void addOneToA(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
